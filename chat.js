88de4fda9cad9b8945c89aab83ad0e3f623ca87a
jienxiya <jeinxiya@gmail.com>
2019-09-23 21:58:09 +0800

    modified

--- chat.js
+++ chat.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
-prends = {};
 
 var active = [];// list of active usernames
 app.get('/', function (req, res) {
@@ -16,23 +15,21 @@ io.on('connection', function (socket) {
   });
 });
 
-var numUsers = 0;
-
 //every connection
 io.on('connection', (socket) => {
   console.log('New user connected')
-  var addedUser = false;
+
+
   //default username
   socket.username = "Anonymous"
-  // io.sockets.emit('online_user', {username: socket.username});
 
 
   //change username
   socket.on('change_username', (data) => {
-    active.push(data.username)//
+    active.push(data.username)
     socket.username = data.username;
     console.log(data.username);
-    io.sockets.emit('change_username', { username: data.username, users: active});
+    io.sockets.emit('change_username', { username: data.username, users: active });
 
   })
 
@@ -50,30 +47,11 @@ io.on('connection', (socket) => {
   socket.on('add user', (username) => {
     if (addedUser) return;
 
-    // storing username
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    })
-
-    //global connection
-    //   socket.broadcast.emit('user joined', {
-    //     username: socket.username,
-    //     numUsers: numUsers
-    //   });
-    //   console.log(numUsers);
-    // });
-
-
-    // socket.on('disconnect', function (data) {
-    //   console.log('user disconnected:' + socket.username)
-    //   if (!socket.username) return;
-    //   prends[socket.username].online = false; //We dont splie nickname from array but change online state to false
-    //   updateNicknames();
-    // });
   });
+
+  // socket.on('disconnect', function() {
+  //     console.log('user disconnected')
+  //   });
 });
 
 http.listen(port, function () {
